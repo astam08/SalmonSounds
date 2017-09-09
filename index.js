58e18068ae2c094d0fa7d6cc4f89d1a773873b1b
iComputer7 <seancale@seancale.tk>
2017-09-08 21:55:53 -0500

    Custom game routine

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const client = Discord.Client();
 //gotta import that fancy config file
 const config = require("config.json").configuration;
 
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+  if (config.custom-game == "") {
+    client.user.setGame(config.prefix + "help");
+  } else {
+    client.user.setGame(config.custom-game);
+  }
+});
+
 client.on("message", (message) => { //eww these indents suck but i'm too lazy to change the setting
   if (message.content == "salmon test") {
     message.channel.send("Yeah, I work. So be it");
