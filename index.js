98e79eb93677565a4a85f687e62e2ae0d9f1fb84
Ty Qualters <Coachqandtybo2@users.noreply.github.com>
2017-09-08 23:23:06 -0500

    test

--- index.js
+++ index.js
@@ -49,8 +49,8 @@ client.on("message", (message) => { //eww these indents suck but i'm too lazy to
   }
   if (message.content.toLowerCase() == config["prefix"] + "disconnect") {
   	if (message.guild.voiceConnection) {
-      if (message.guild.voiceConnection.dispatcher) {
-  		    message.guild.voiceConnection.dispatcher.end();
+      if (message.guild.voiceConnection) {
+  		    message.guild.voiceConnection.disconnect();
       }
   	} else {
   		message.reply("I am not in a voice channel!");
