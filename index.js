15fd2639e0651909143b755ee5653c5545a5936a
iComputer7 <seancale@seancale.tk>
2017-09-08 21:48:00 -0500

    On message event listener
    sarcastic code rocks

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const Discord = require("discord.js");
 const client = Discord.Client();
+
+client.on("message", (message) => { //eww these indents suck but i'm too lazy to change the setting
+  if (message.content == "salmon test") {
+    message.channel.send("Yeah, I work. So be it");
+  }
+});
