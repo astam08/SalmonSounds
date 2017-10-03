25ce813beb89bf2b909670eb8850607d7f664100
iComputer7 <seancale@seancale.tk>
2017-10-03 17:20:26 -0500

    Fixing ty's shitty "queue"
    This reverts commit 8e145f271ab5aa9f4ac329edad64064bcf306ed9.

--- index.js
+++ index.js
@@ -70,6 +70,7 @@ client.on("message", (message) => { //eww these indents suck but i'm too lazy to
   }
 
   if (message.content.toLowerCase().startsWith(config["prefix"] + "play")) {
+<<<<<<< HEAD
     if(guildQueue[message.guild.id]){
       var parser = message.content.split(" "), parsed = [];
       for (let i = 0; i <= parser.length; i++) {
@@ -117,24 +118,47 @@ client.on("message", (message) => { //eww these indents suck but i'm too lazy to
             if (i >= 1) {
             	parsed.push(parser[i]);		//Horrible parser because javascript sucks
             }
+=======
+    if (message.member.voiceChannel) {
+      if (message.member.voiceChannel.joinable) {
+        var parser = message.content.split(" "), parsed = [];
+        for (let i = 0; i <= parser.length; i++) {
+          if (i >= 1) {
+          	parsed.push(parser[i]);		//Horrible parser because javascript sucks
+>>>>>>> parent of 8e145f2... You are already playing something
           }
-          parsed = parsed.join(" ");
-          parsed = parsed.substring(0, parsed.length - 1);
+        }
+        parsed = parsed.join(" ");
+        parsed = parsed.substring(0, parsed.length - 1);
 
-          //Parser debug
-          //message.channel.send(parsed);
-          try {
-            var stream = ytdl(parsed, {filter: "audioonly"});
-            ytdl.getInfo(parsed).then((i, f) => {
-              message.channel.send({embed: {
-                color: 16753920,
-                thumbnail: {
-                  url: i["iurl"]
+        //Parser debug
+        //message.channel.send(parsed);
+        try {
+          var stream = ytdl(parsed, {filter: "audioonly"});
+          ytdl.getInfo(parsed).then((i, f) => {
+            message.channel.send({embed: {
+              color: 16753920,
+              thumbnail: {
+                url: i["iurl"]
+              },
+              author: {
+                name: client.user.username,
+                icon_url: client.user.displayAvatarURL,
+              },
+              title: i["title"],
+              url: i["video_url"],
+              fields: [
+                {
+                  name: "Author",
+                  value: i["author"]["name"],
+                  inline: true
                 },
-                author: {
-                  name: client.user.username,
-                  icon_url: client.user.displayAvatarURL,
+                {
+                  name: "Length (seconds)",
+                  value: i["length_seconds"],
+                  inline: true
                 },
+<<<<<<< HEAD
                 title: i["title"],
                 url: i["video_url"],
                 fields: [
@@ -177,16 +201,28 @@ client.on("message", (message) => { //eww these indents suck but i'm too lazy to
               }
               tryMusic(stream);
               });
+=======
+                {
+                  name: "Views",
+                  value: i["view_count"],
+                  inline: true
+                }
+              ]
+            }});
+            message.member.voiceChannel.join().then((connection) => {
+              message.channel.send("Joining `" + message.member.voiceChannel.name + "`");
+              connection.playStream(stream).on("end", ()=> {connection.disconnect();});
+>>>>>>> parent of 8e145f2... You are already playing something
             });
-          } catch (e) {
-            message.channel.send(e.message);
-          }
-        } else {
-          message.reply("It seems that you are in a voice channel, but I can't join!");
+          });
+        } catch (e) {
+          message.channel.send(e.message);
         }
       } else {
-        message.reply("You are not in a voice channel!");
+        message.reply("It seems that you are in a voice channel, but I can't join!");
       }
+    } else {
+      message.reply("You are not in a voice channel!");
     }
   }
   if(message.content.toLowerCase() == config['prefix'] + 'clear'){
