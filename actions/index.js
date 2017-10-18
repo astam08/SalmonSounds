b6eb99c7221b60afdb979ab9c5855a81e2361be8
Ty Qualters <12106131+SalmonSeasoning@users.noreply.github.com>
2017-10-18 15:42:27 -0500

    Update index.js

--- actions/index.js
+++ actions/index.js
@@ -1,5 +1,4 @@
 module.exports = {
   'yt_search': require('./actions/youtube-api/stream.js'),
-  'parser': require('./actions/discord/parser.js'),
-  'commands': require('./actions/discord/commands.js')
+  'parser': require('./actions/discord/parser.js')
 };
